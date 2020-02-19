02dbaffbcd749ad7e555365f38fb4aa961b9f634
EuSouMrJ <rooseveltjusto@gmail.com>
2020-02-18 21:00:40 -0500

    Comments

--- doe/server.js
+++ doe/server.js
@@ -1,10 +1,13 @@
+// configurando o servidor
 const express = require("express")
 const server = express()
 
+// configurar a apresentação da página
 server.get("/", function(req, res){
   return res.send("ok, cheguei aqui com o nodemon!")
 })
 
+// ligar o servidor e permitir o acesso na porta 3000
 server.listen(3000, function(){
   console.log("Iniciei o servidor.")
 })
