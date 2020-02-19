6348da75ceb3246f356d29dace5bc8fce3c60012
EuSouMrJ <rooseveltjusto@gmail.com>
2020-02-18 21:37:43 -0500

    post data from form

--- doe/public/server.js
+++ doe/public/server.js
@@ -6,6 +6,9 @@ const server = express()
 
 server.use(express.static('public'))
 
+// habilitar body do formulário
+server.use(express.urlencoded({ extended: true }))
+
 // configurando a template engine
 const nunjucks = require('nunjucks')
 nunjucks.configure("./", {
@@ -36,6 +39,21 @@ const donors = [
 // configurar a apresentação da página
 server.get("/", function(req, res){
   return res.render("index.html", {donors})
+})
+
+server.post("/", function(req, res){
+  // obter dados do form
+  const name = req.body.name
+  const email = req.body.email
+  const blood = req.body.blood
+
+  // injeta valores no array
+  donors.push({
+    name: name,
+    blood: blood,
+  })
+
+  return res.redirect("/")
 })
 
 // ligar o servidor e permitir o acesso na porta 3000
